ce80da2d89e21ca785bdd325f08dce1fca1dd57b
verma hash <vermaharsh1010@gmail.com>
2025-08-08 20:49:55 -0400

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Harsh Verma',
+    author_emails = 'hverma4@pride.hofstra.edu',
+    packages = find_packages(),
+    istall_requires= []
+    )
